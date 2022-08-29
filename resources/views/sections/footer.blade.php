bb33eaa5fd713bd3d0f914c47a284ed0e46f04aa
waner1er <erwan@derniercri.io>
2022-08-29 17:54:39 +0200

    update: edit the footer

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -6,13 +6,14 @@
     </div>
     <div class="footer__first-row__contact-me">
       @php(dynamic_sidebar('sidebar-footer'))
-      <x-contact-me contact="{{get_bloginfo('admin_email')}}"></x-contact-me>
+{{--      <x-contact-me contact="{{get_bloginfo('admin_email')}}"></x-contact-me>--}}
     </div>
   </div>
   <x-pictos.back-to-top/>
   <div class="footer__second-row">
-    @php(wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'footer_navigation']) )
-
+    <div class="wrapper">
+      @php(wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'footer_navigation']) )
+    </div>
   </div>
   <div class="footer__third-row">&#xA9; Erwan RIVET {{ get_the_date( 'Y' ) }}</div>
 </footer>
